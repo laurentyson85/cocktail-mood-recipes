fa43ab1d2c1e70714164e65f4013f6c76f401cc3
Lauren Tyson <lauren.wlkr@gmail.com>
2022-06-27 15:57:11 -0600

    Add HTML Element variables to JS in DOM load

--- js/index.js
+++ js/index.js
@@ -1,10 +1,26 @@
-const errorBanner = document.querySelector("#error-banner")
-const moodBtn = document.querySelector("#mood-btn")
-const spiritBtn = document.querySelector("spirit-btn")
-const recipeSection = document.querySelector("cocktail-recipe")
-const favorites = document.querySelectorAll("favorites")
+     
+document.addEventListener("DOMContentLoaded", () => {
+    const errorBanner = document.querySelector("#error-banner")
+    const moodBtn = document.querySelector("#mood-btn")
+    const spiritBtn = document.querySelector("spirit-btn")
+    const recipeSection = document.querySelector("cocktail-recipe")
+    const favorites = document.querySelectorAll("favorites")
+
+    function handleMood(event){
+        console.log(event)
+    }
+
+    moodBtn.addEventListener("click", handleMood)  
+
+ })
 
 
-document.addEventListener("DOMContentLoaded"(){
-    
-})
+function handleMood(event){
+ console.log(event)
+}
+
+
+
+
+
+
