6b0673613b216d97dd310b8e47aacef60e6639e3
Lauren Tyson <lauren.wlkr@gmail.com>
2022-06-27 17:12:41 -0600

    Add event listeners to hover buttons

--- js/index.js
+++ js/index.js
@@ -1,23 +1,27 @@
+ 
+document.addEventListener("DOMContentLoaded", () => { 
      
-document.addEventListener("DOMContentLoaded", () => {
-    const errorBanner = document.querySelector("#error-banner")
-    const moodBtn = document.querySelector("#mood-btn")
-    const spiritBtn = document.querySelector("spirit-btn")
-    const recipeSection = document.querySelector("cocktail-recipe")
-    const favorites = document.querySelectorAll("favorites")
 
-    function handleMood(event){
-        console.log(event)
-    }
+     const errorBanner = document.querySelector("#error-banner")
+     const moodBtn = document.querySelector("#mood-btn")
+     const spiritBtn = document.querySelector("spirit-btn")
+     const recipeSection = document.querySelector("cocktail-recipe")
+     const favorites = document.querySelectorAll("favorites")
 
-    moodBtn.addEventListener("click", handleMood)  
+    document.addEventListener('click', function (event) {
 
- })
+        if (event.target.matches('.moodDropBtn')) {
+            console.log(event)
+        }
+    
+        if (event.target.matches('.spiritDropBtn')) {
+            console.log(event)
+    
+        }   
+    })   
+})
 
 
-function handleMood(event){
- console.log(event)
-}
 
 
 
