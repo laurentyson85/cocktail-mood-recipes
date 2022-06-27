1164efba53b7373a8aa92e1f0000c3b1fecab3e2
Lauren Tyson <lauren.wlkr@gmail.com>
2022-06-27 15:07:59 -0600

    Hide Spirit button spirit-btn element

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 const errorBanner = document.querySelector("#error-banner")
 const moodBtn = document.querySelector("#mood-btn")
 const spiritBtn = document.querySelector("spirit-btn")
+const recipeSection = document.querySelector("cocktail-recipe")
+const favorites = document.querySelectorAll("favorites")
+
+
+document.addEventListener("DOMContentLoaded"(){
+    
+})
