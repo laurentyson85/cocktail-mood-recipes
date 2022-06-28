1b4768f0cd1a5490b5a6e1b1b90fa25f80235ad1
Lauren Tyson <lauren.wlkr@gmail.com>
2022-06-27 19:32:21 -0600

    Add API Fetch

--- js/index.js
+++ js/index.js
@@ -2,20 +2,32 @@
 document.addEventListener("DOMContentLoaded", () => { 
      
 
-     const errorBanner = document.querySelector("#error-banner")
-     const moodBtn = document.querySelector("#mood-btn")
-     const spiritBtn = document.querySelector("spirit-btn")
-     const recipeSection = document.querySelector("cocktail-recipe")
-     const favorites = document.querySelectorAll("favorites")
+     let errorBanner = document.querySelector("#error-banner")
+     let moodBtn = document.querySelector(".moodDropDownBtn")
+     let spiritBtn = document.querySelector(".spiritDropDownBtn")
+     let recipeSection = document.querySelector("cocktail-recipe")
+     let favorites = document.querySelectorAll("favorites")
+     let mood
+     let spirit
 
-    document.addEventListener('click', function (event) {
 
+    document.addEventListener('click', function (event) {      
         if (event.target.matches('.moodDropBtn')) {
-            console.log(event)
+           mood = event.target.innerText
+           moodBtn.hidden = true
+           spiritBtn.hidden = false
+           console.log(mood)    
         }
     
-        if (event.target.matches('.spiritDropBtn')) {
-            console.log(event)
+        if (event.target.matches('.spiritDropBtn')) {            
+            spirit = event.target.innerText
+            console.log(spirit)
+            // fetch("www.thecocktaildb.com/api/json/v1/1/filter.php?i=")
+            // .then(response => response.json())
+            // .then(data => {
+            //     data.map(data => renderMonster(data))
+            // })
+            
     
         }   
     })   
