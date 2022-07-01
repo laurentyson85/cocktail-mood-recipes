df52bfcb5c67945881e84b676a2d5819130c756d
Lauren Tyson <lauren.wlkr@gmail.com>
2022-07-01 16:17:06 -0600

    Refactor for loop for ingredients to .map

--- js/index.js
+++ js/index.js
@@ -40,84 +40,92 @@ document.addEventListener("DOMContentLoaded", () => {
             fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailId}`)
             .then(response => response.json())
             .then(data => {
-                fullRecipe = data.drinks[0]                    
-
-                ingredients = Object.entries(data.drinks[0]).slice(17,31).map(entry => entry[1]).filter(element => element !== null)                  
-            
-                measurements = Object.entries(data.drinks[0]).slice(32,46).map(entry => entry[1]).filter(element => element !== null) 
-                
-                //think about refactoring with a map                                                    
-                for (let i=0; i< measurements.length; i++){
-                    let ingredientRow = `${measurements[i]} ${ingredients[i]}`                     
-                    allIngredients.push(ingredientRow)                        
-                }
-
+                buildIngredients(data)
                 renderCocktail()
-        }  ) 
+            }) 
+        })
+    }
 
-    })
+function buildIngredients(data){
+    fullRecipe = data.drinks[0]                    
 
+    ingredients = Object.entries(data.drinks[0]).slice(17,31).map(entry => entry[1]).filter(element => element !== null)                  
+
+    measurements = Object.entries(data.drinks[0]).slice(32,46).map(entry => entry[1]).filter(element => element !== null) 
+
+    let ingredientRow  = measurements.map((row) => row.ingredients)
+    allIngredients.push(ingredientRow)
+            
+    //think about refactoring with a map                                                    
+    // for (let i=0; i< measurements.length; i++){
+    //     let ingredientRow = `${measurements[i]} ${ingredients[i]}`                     
+    //     allIngredients.push(ingredientRow)
+        
+        
+    // }
 }
                 
         
-        function renderCocktail(){
-            const mainDiv = document.createElement("div")
-            mainDiv.classList.add("card")
-            mainDiv.id = fullRecipe.idDrink
+    function renderCocktail(){
+        const mainDiv = document.createElement("div")
+        mainDiv.classList.add("card")
+        mainDiv.id = fullRecipe.idDrink
 
-            const secondDiv = document.createElement("div")
-            secondDiv.classList.add("container")
-            secondDiv.id = "drinkContainer"
+        const secondDiv = document.createElement("div")
+        secondDiv.classList.add("container")
+        secondDiv.id = "drinkContainer"
 
 
-            const btn = document.createElement("button")
-            btn.classList.add("expand")
-            btn.id = "expand"
-            btn.textContent = "Expand for details"   
-          
-            btn.addEventListener("mouseenter",(handleOver))
-            btn.addEventListener("mouseleave", (handleLeave))
-            
-            
-            const img = document.createElement("img")
-            img.src = fullRecipe.strDrinkThumb
-            img.id = "drinkImg"
-            img.classList.add("thumbnail")
+        const btn = document.createElement("button")
+        btn.classList.add("expand")
+        btn.id = "expand"
+        btn.textContent = "Expand for details"   
+        
+        btn.addEventListener("mouseenter",(handleOver))
+        btn.addEventListener("mouseleave", (handleLeave))
+        
+        
+        const img = document.createElement("img")
+        img.src = fullRecipe.strDrinkThumb
+        img.id = "drinkImg"
+        img.classList.add("thumbnail")
 
-            const h2 = document.createElement("h2")
-            h2.textContent = `you should have a "${fullRecipe.strDrink}"`
+        const h2 = document.createElement("h2")
+        h2.textContent = `you should have a "${fullRecipe.strDrink}"`
 
-            const selectedMood= document.createElement("p")
-            selectedMood.textContent = `Since you feel ${mood.toLowerCase()},` 
+        const selectedMood= document.createElement("p")
+        selectedMood.textContent = `Since you feel ${mood.toLowerCase()},` 
 
-            const thirdDiv = document.createElement("div")
-            thirdDiv.id = "cardInfo"
-            thirdDiv.classList.add("hide")           
+        const thirdDiv = document.createElement("div")
+        thirdDiv.id = "cardInfo"
+        thirdDiv.classList.add("hide")           
 
-            const p = document.createElement('p')            
-            p.textContent = `Ingredients: ${allIngredients.join(", ")}`   
+        const p = document.createElement('p')            
+        p.textContent = `Ingredients: ${allIngredients.join(", ")}`   
 
-            const br = document.createElement("br")   
-            
-            const br2 = document.createElement("br")
+        const br = document.createElement("br")   
+        
+        const br2 = document.createElement("br")
 
-            const br3 = document.createElement("br")             
-                    
-            const instructions = document.createElement("p")
-            instructions.textContent = `Instructions: ${fullRecipe.strInstructions}`  
-            
-            const button = document.createElement("button")
-            button.classList.add("newDrink")
-            button.id = "newDrink"
-            button.textContent = "Select a new drink" 
-            button.addEventListener("click",(handleClick))               
+        const br3 = document.createElement("br")             
+                
+        const instructions = document.createElement("p")
+        instructions.textContent = `Instructions: ${fullRecipe.strInstructions}`  
+        
+        const button = document.createElement("button")
+        button.classList.add("newDrink")
+        button.id = "newDrink"
+        button.textContent = "Select a new drink" 
+        button.addEventListener("click",(handleClick))               
 
-            thirdDiv.append(p, br, instructions)                
-            secondDiv.append(img, selectedMood, h2, br2, btn)
-            mainDiv.append(secondDiv, thirdDiv)
-            recipeSection.append(button, br3, mainDiv)
-            favorites.hidden = false
-        }     
+        thirdDiv.append(p, br, instructions)                
+        secondDiv.append(img, selectedMood, h2, br2, btn)
+        mainDiv.append(secondDiv, thirdDiv)
+        recipeSection.append(button, br3, mainDiv)
+        favorites.hidden = false
+    } 
+    
+    
 
         //think about how to write these as arrow functions. add prevent default on everything.
 
@@ -156,3 +164,5 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 })
+
+
